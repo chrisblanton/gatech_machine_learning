66f6e692aa7dd5bc4b886c3a0fc6597581717752
Christopher Blanton <chris.blanton@gatech.edu>
2020-08-26 15:09:44 -0400

    CB: Fixed issue with start_workshop.sh

--- start_workshop.sh
+++ start_workshop.sh
@@ -3,4 +3,4 @@
 
 pace-jupyter-notebook -l walltime=01:00:00 -l nodes=1:ppn=1 -q pace-training \
     --anaconda=anaconda3 \
-    --conda-env=/storage/home/hcodaman1/cblanton7/coda-data/shared/envs/pace_mlws_py37
+    --conda-env=/storage/coda1/pace-admins/cblanton7/shared/envs/pace_mlws_py37
